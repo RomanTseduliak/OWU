7460fb8c3b80cb4d3c1a5f2924b68b84b5ed315c
RomanTseduliak <alligarch@gmail.com>
2021-02-05 14:58:06 +0200

    addMyForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,27 @@
 import { Component, OnInit } from "@angular/core";
 import { AbstractControl, FormControl, FormGroup } from "@angular/forms";
-import * as buffer from "buffer";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-  form: FormGroup;
-
   constructor() {}
 
-  ngOnInit(): void {
-    this.form = new FormGroup({
-      name: new FormControl(""),
-      age: new FormControl("", [this.ageValidator]),
-    });
-  }
-
+  ngOnInit(): void {}
+  myForm = new FormGroup({
+    name: new FormControl(""),
+    age: new FormControl("", [this.ageValidator]),
+  });
   ageValidator(control: AbstractControl): { ageError: boolean } | null {
-    if (control.value < 15) {
+    if (control.value < 5) {
       return { ageError: true };
     }
     return null;
   }
 
-  send(form: FormGroup): void {
-    console.log(form);
+  send(myForm: FormGroup): void {
+    console.log(myForm.value);
   }
 }
